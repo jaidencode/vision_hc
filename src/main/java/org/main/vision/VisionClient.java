619945eb580296493423b0d36ee0fddcba6a4ace
jaidencode <33370705+jaidencode@users.noreply.github.com>
2025-06-22 23:10:57 -0400

    Implement basic toggleable speed hack

--- src/main/java/org/main/vision/VisionClient.java
+++ src/main/java/org/main/vision/VisionClient.java
@@ -1,2 +1,30 @@
-package org.main.vision;public class VisionClient {
+package org.main.vision;
+
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.InputEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import org.main.vision.actions.SpeedHack;
+
+/**
+ * Handles client-only events.
+ */
+@Mod.EventBusSubscriber(modid = "vision", value = Dist.CLIENT)
+public class VisionClient {
+    private static final SpeedHack SPEED_HACK = new SpeedHack();
+
+    static void init() {
+        VisionKeybind.register();
+    }
+
+    public static SpeedHack getSpeedHack() {
+        return SPEED_HACK;
+    }
+
+    @SubscribeEvent
+    public static void onKeyInput(InputEvent.KeyInputEvent event) {
+        if (VisionKeybind.speedKey.isDown()) {
+            SPEED_HACK.toggle();
+        }
+    }
 }
--- src/main/java/org/main/vision/VisionKeybind.java
+++ src/main/java/org/main/vision/VisionKeybind.java
@@ -1,2 +1,17 @@
-package org.main.vision;public class VisionKeybind {
+package org.main.vision;
+
+import net.minecraft.client.settings.KeyBinding;
+import net.minecraftforge.fml.client.registry.ClientRegistry;
+import org.lwjgl.glfw.GLFW;
+
+/**
+ * Holds keybindings for Vision mod.
+ */
+public class VisionKeybind {
+    public static KeyBinding speedKey;
+
+    static void register() {
+        speedKey = new KeyBinding("key.vision.speed", GLFW.GLFW_KEY_O, "key.categories.vision");
+        ClientRegistry.registerKeyBinding(speedKey);
+    }
 }
